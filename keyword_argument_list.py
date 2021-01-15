8d001e30f906ff7accd6971e5192ff72e2658307
Mr.Simple <simple@Mr.simple>
2021-01-15 10:08:56 +0700

    belajar keyword argument list

--- keyword_argument_list.py
+++ keyword_argument_list.py
@@ -1,6 +1,19 @@
-def create_html(tag, text):
-    html = f"<{tag}>{text}</{tag}>"
+# def create_html(tag, text, href=""):
+#     html = f"<{tag} href='{href}'>{text}</{tag}>"
+#     return html
+
+
+def create_html(tag, text, **attributes):
+    html = f"<{tag}"
+
+    for key, value in attributes.items():
+        html = html + f" {key}='{value}'"
+
+    html = html + f">{text}</{tag}>"
+    print(attributes)
     return html
 
-html = create_html("p", "Hello Python")
+html = create_html("p", "Hello Python", style="paragraf")
+print(html)
+html = create_html("p", "ini link", href="www.google.com")
 print(html)
